f00763d30d495e9668e0f5cadcc356075194e5da
Jasper Fennet <jasper.fennet@gmail.com>
2021-02-04 22:03:14 +0100

    documentation, test, memdb

---
+++ src/main/java/nl/jasperfennet/springapplicationtemplate/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package nl.jasperfennet.springapplicationtemplate.controller;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloWorldController {
+
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User greeted properly", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Name not provided"),
+    })
+    public String helloWorld(@Parameter(description = "Name used in greetings") @RequestParam String name) {
+        return "Hello " + name;
+    }
+}
--- src/test/java/nl/jasperfennet/springapplicationtemplate/SpringApplicationTemplateApplicationTests.java
+++ src/test/java/nl/jasperfennet/springapplicationtemplate/SpringApplicationTemplateApplicationTests.java
@@ -3,11 +3,14 @@ package nl.jasperfennet.springapplicationtemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringApplicationTemplateApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
