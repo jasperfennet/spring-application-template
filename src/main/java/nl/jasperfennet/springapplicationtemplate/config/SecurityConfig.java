f4c7f5448cf04d902b7c9d124322aa23532e9353
Jasper Fennet <jasper.fennet@gmail.com>
2021-02-06 18:13:25 +0100

    security

---
+++ src/main/java/nl/jasperfennet/springapplicationtemplate/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package nl.jasperfennet.springapplicationtemplate.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jasper").password(passwordEncoder().encode("jasper"))
+                .authorities("ROLE_ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**/open-api**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**/actuator/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
