430434df74c355cff45a6f923e7b82703922fd33
Jasper Fennet <jasper.fennet@gmail.com>
2021-08-11 23:25:53 +0200

    cleanup application specific code

--- src/main/java/nl/jasperfennet/springapplicationtemplate/config/SecurityConfig.java
+++ src/main/java/nl/jasperfennet/springapplicationtemplate/config/SecurityConfig.java
@@ -6,41 +6,16 @@
 
 package nl.jasperfennet.springapplicationtemplate.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin"))
-                .authorities("ROLE_ADMIN")
-                .and()
-                .withUser("user").password(passwordEncoder().encode("user"))
-                .authorities("ROLE_USER");
-    }
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/open-api/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
-                .anyRequest().authenticated();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                .antMatchers("/**").permitAll();
     }
 }
